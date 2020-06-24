69bd29cbca9316aca131aab2f45370f725912367
conor <conor@irrelevantbeats.com>
2020-06-24 23:01:20 +0100

    basic REST endpoints for topic

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -16,30 +16,8 @@ defmodule DiscussWeb.Router do
   scope "/", DiscussWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
-
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
+    get "/", TopicController, :index
+    resources "/topics", TopicController
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", DiscussWeb do
-  #   pipe_through :api
-  # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: DiscussWeb.Telemetry
-    end
-  end
 end
